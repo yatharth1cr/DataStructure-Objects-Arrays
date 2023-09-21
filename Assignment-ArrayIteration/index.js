066595891526f7a6e00b9636609357c5f8b51690
yatharth1cr <yatharthgiri187@gmail.com>
2023-09-21 13:41:43 +0530

    added a file of array interation

---
+++ Assignment-ArrayIteration/index.js
@@ -0,0 +1,18 @@
+// 1. Create an array named numbers and store 5 number values in it
+let numbers = [1, 15, 12, 9, 2];
+// 2. Calculate the sum of array items and print it to the console using console.log()
+let sum = 0;
+for (let i = 1; i <= numbers.length; i++) {
+  sum += numbers[i];
+}
+console.log(sum);
+// 3. Calculate the average of array items and print it to the console using console.log()
+let average = sum / numbers.length;
+console.log(average);
+// 4. Find the highest number in the array and print it to the console using console.log()
+// 5. Find the lowest number in the array and print it to the console using console.log()
+// 6. Find the even numbers in the array and print them to the console using console.log()
+// 7. Find the odd numbers in the array and print them to the console using console.log()
+// 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
+// 9. Log all the element of the array one by one
+// 10. Find all the number in the array that is divisible by 3
